6175297579faf2591eb156323f6afe29eac933de
ehmatthes <sophia.world1992@gmail.com>
2017-11-26 00:05:44 -0500

    update

--- SI507_project6.py
+++ SI507_project6.py
@@ -1,6 +1,7 @@
 # Import statements
 import psycopg2
 import psycopg2.extras
+from psycopg2 import sql
 from config import *
 import sys
 import csv 
@@ -31,101 +32,135 @@ conn, cur = get_connection_and_cursor()
 
 
 # Write code / functions to create tables with the columns you want and all database setup here.
-cur.execute('DROP TABLE IF EXISTS "Sites"')
-cur.execute('DROP TABLE IF EXISTS "States"')
+def setup_database():
+    cur.execute('DROP TABLE IF EXISTS "Sites"')
+    cur.execute('DROP TABLE IF EXISTS "States"')
 
-cur.execute(""" CREATE TABLE IF NOT EXISTS "States"(
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(40) NOT NULL UNIQUE
-)""")
+    cur.execute(""" CREATE TABLE IF NOT EXISTS "States"(
+        "ID" SERIAL PRIMARY KEY,
+        "Name" VARCHAR(40) NOT NULL UNIQUE
+    )""")
 
-cur.execute(""" CREATE TABLE IF NOT EXISTS "Sites"(
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(128) NOT NULL UNIQUE,
-    Type VARCHAR(128),
-    State_ID INTEGER REFERENCES "States"(ID),
-    Location VARCHAR(255),
-    Description TEXT
-)""")
+    cur.execute(""" CREATE TABLE IF NOT EXISTS "Sites"(
+        "ID" SERIAL PRIMARY KEY,
+        "Name" VARCHAR(128) NOT NULL UNIQUE,
+        "Type" VARCHAR(128),
+        "State_ID" INTEGER REFERENCES "States"("ID"),
+        "Location" VARCHAR(255),
+        "Description" TEXT
+    )""")
 
-conn.commit()
-print('Setup database complete')
+    conn.commit()
+    print('Setup database complete')
 
 
 
 # Write code / functions to deal with CSV files and insert data into the database here.
-# insert States Data
-cur.execute("""INSERT INTO "States" (Name) VALUES('Arkansas')""")
-cur.execute(""" INSERT INTO "States" (Name) VALUES('California') """)
-cur.execute(""" INSERT INTO "States" (Name) VALUES('Michigan') """)
-conn.commit()
-print('Insert data into Sates Table')
+
+def insert(connection, cursor, table, data_dict, no_return = True):
+    column_names = data_dict.keys()
+
+    if not no_return:
+        query = sql.SQL('INSERT INTO "{0}"({1}) VALUES({2}) ON CONFLICT DO NOTHING RETURNING "ID"').format(
+            sql.SQL(table),
+            sql.SQL(', '). join(map(sql.Identifier, column_names)),
+            sql.SQL(', ').join(map(sql.Placeholder, column_names))
+        )
+
+    else:
+        query = sql.SQL('INSERT INTO "{0}"({1}) VALUES({2}) ON CONFLICT DO NOTHING').format(
+            sql.SQL(table),
+            sql.SQL(', '). join(map(sql.Identifier, column_names)),
+            sql.SQL(', ').join(map(sql.Placeholder, column_names))
+        )
+
+    sql_string = query.as_string(connection)
+    #print(sql_string)
+    cursor.execute(sql_string, data_dict)
+
+    if not no_return:
+        print (cursor.fetchone()["ID"])
 
 
-# get state_id from States
+
+def get_site_dict(site_list,state_id):
+    return {
+    "State_ID" : state_id,
+    "Name" : site_list[0],
+    "Type" : site_list[2],
+    "Location" : site_list[1],
+    "Description" : site_list[4]
+    }
+
 def get_state_id(state_name):
-    cur.execute(""" SELECT ID FROM "States" WHERE Name=%s""", (state_name,))
+    cur.execute(""" SELECT "ID" FROM "States" WHERE "Name"=%s""", (state_name,))
     result = cur.fetchone()
-    state_id = result["id"]
+    state_id = result["ID"]
     return state_id
-
-
-# insert Sites Data
-def csv_to_db(file_name,state_name):
-    f = open(file_name,"r") 
-    reader = csv.DictReader(f)
-
-    state_id = get_state_id(state_name)
-
-    for row in reader:
-        cur.execute(""" INSERT INTO
-            "Sites" (Name,Type,State_ID,Location,Description)
-            VALUES(%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING """,
-            (row["NAME"],row["TYPE"],state_id,row["LOCATION"],row["DESCRIPTION"]))
-        
-    conn.commit()
-    print('Insert data in the {} into Sites table'.format(file_name))
 
 
 # Make sure to commit your database changes with .commit() on the database connection.
 # Write code to be invoked here (e.g. invoking any functions you wrote above)
 
-csv_to_db('arkansas.csv','Arkansas')
-csv_to_db('california.csv','California')
-csv_to_db('michigan.csv','Michigan')
+if __name__ == '__main__':
+    
+    setup_database()
+
+    # insert States data
+    cur.execute("""INSERT INTO "States" ("Name") VALUES('arkansas')""")
+    cur.execute(""" INSERT INTO "States" ("Name") VALUES('california') """)
+    cur.execute(""" INSERT INTO "States" ("Name") VALUES('michigan') """)
+    conn.commit()
 
 
-# Write code to make queries and save data in variables here.
+    #Insert Sites data
+    csv_list = ['arkansas.csv', 'california.csv', 'michigan.csv']
+    for csv_file in csv_list:
+        state_name = csv_file.split('.')[0]
+        #print(state_name)
+        state_id = get_state_id(state_name)
+        with open(csv_file, 'r') as f:
+            reader = csv.reader(f)
+            first_row = next(reader)
+            for row in reader:
+                site_dict = get_site_dict(row, state_id)
+                insert(conn, cur, "Sites", site_dict)
+
+    conn.commit()
+
+
 #query the database for all of the locations of the sites. Save the resulting data in a variable called all_locations.
 print("---Query the database for all of the locations of the sites.---")
-cur.execute("""SELECT Location FROM "Sites" """)
+cur.execute("""SELECT "Location" FROM "Sites" """)
 all_locations = cur.fetchall()
 print(all_locations)
 
 #query the database for all of the names of the sites whose descriptions include the word beautiful. Save the resulting data in a variable called beautiful_sites.
 print("---Query the database for the total number of sites whose type is National Lakeshore---")
-cur.execute("""SELECT Name FROM "Sites" WHERE Description ILIKE '%beautiful%' """)
+cur.execute("""SELECT "Name" FROM "Sites" WHERE "Description" ILIKE '%beautiful%' """)
 beautiful_sites = cur.fetchall()
 print(beautiful_sites)
 
 #query the database for the total number of sites whose type is National Lakeshore. Save the resulting data in a variable called natl_lakeshores.
 print("---Query your database for the names of all the national sites in Michigan---")
-cur.execute("""SELECT COUNT(*) FROM "Sites" WHERE Type = 'National Lakeshore' """)
+cur.execute("""SELECT COUNT(*) FROM "Sites" WHERE "Type" = 'National Lakeshore' """)
 natl_lakeshores = cur.fetchall()
 print(natl_lakeshores)
 
 #query your database for the names of all the national sites in Michigan. Save the resulting data in a variable called michigan_names
 print("---Query your database for the names of all the national sites in Michigan---")
-cur.execute("""SELECT "Sites".Name FROM "Sites" INNER JOIN "States" ON ("Sites".State_ID = "States".ID) WHERE "States".Name='Michigan' """)
+cur.execute("""SELECT "Sites"."Name" FROM "Sites" INNER JOIN "States" ON ("Sites"."State_ID" = "States"."ID") WHERE "States"."Name"='michigan' """)
 michigan_names = cur.fetchall()
 print(michigan_names)
 
 #query your database for the total number of sites in Arkansas. Save the resulting data in a variable called total_number_arkansas.
 print("---Query your database for the total number of sites in Arkansas---")
-cur.execute(""" SELECT COUNT(*) FROM "Sites" INNER JOIN "States" ON ("Sites".State_ID = "States".ID) WHERE "States".Name='Arkansas' """)
+cur.execute(""" SELECT COUNT(*) FROM "Sites" INNER JOIN "States" ON ("Sites"."State_ID" = "States"."ID") WHERE "States"."Name"='arkansas' """)
 total_number_arkansas = cur.fetchall()
 print(total_number_arkansas)
 
 # We have not provided any tests, but you could write your own in this file or another file, if you want.
+
+
 
 
